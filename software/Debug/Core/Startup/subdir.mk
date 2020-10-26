06f4820ad4a0cb34fbe7752f80dfa6a99d60d56b
Alex Lossberg <50721703+LDwhoRU@users.noreply.github.com>
2020-10-26 19:59:50 +1000

    Init code base

---
+++ software/Debug/Core/Startup/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/Startup/startup_stm32f765vitx.s 
+
+OBJS += \
+./Core/Startup/startup_stm32f765vitx.o 
+
+S_DEPS += \
+./Core/Startup/startup_stm32f765vitx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Startup/startup_stm32f765vitx.o: ../Core/Startup/startup_stm32f765vitx.s
+	arm-none-eabi-gcc -mcpu=cortex-m7 -g3 -c -x assembler-with-cpp -MMD -MP -MF"Core/Startup/startup_stm32f765vitx.d" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
