75cad278de81564c319cfe03f18746dfff61f6f9
Tom Fraser <tr.fraser@bigpond.com>
2021-08-15 15:20:03 +1000

    Adding Toque Vectoring

--- software/Core/Inc/inverter_vesc.h
+++ software/Core/Inc/inverter_vesc.h
@@ -29,6 +29,7 @@
 void vesc_send_shutdown();
 void vesc_update_enabled(bool state);
 void vesc_send_pedals(uint16_t accel, uint16_t brake);
+void vesc_torque_vectoring(double steeringAngle, double* fl, double* fr, double* rl, double* rr);
 void vesc_request_motor_amps();
 
 #endif /* INC_INVERTER_VESC_H_ */
